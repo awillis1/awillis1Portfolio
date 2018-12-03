6e9f325852e276f8f7d9cde722d7cb937cff89f8
Student <student@CVS-R107D036.nic.bc.ca>
2018-12-03 13:02:05 -0800

    changes portfolio page to only show links to posts

--- wp-content/themes/twentyseventeen-child/single.php
+++ wp-content/themes/twentyseventeen-child/single.php
@@ -15,13 +15,12 @@ get_header(); ?>
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
 			
-				get_template_part( 'template-parts/post/content', get_post_format() );
+				get_template_part( 'template-parts/post/content', 'single' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
--- wp-content/themes/twentyseventeen-child/template-parts/post/content.php
+++ wp-content/themes/twentyseventeen-child/template-parts/post/content.php
@@ -50,20 +50,7 @@
 	<?php endif; ?>
 
 	<div class="entry-content">
-		<?php
-		/* translators: %s: Name of current post */
-		the_content( sprintf(
-			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-			get_the_title()
-		) );
 
-		wp_link_pages( array(
-			'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
-			'after'       => '</div>',
-			'link_before' => '<span class="page-number">',
-			'link_after'  => '</span>',
-		) );
-		?>
 	</div><!-- .entry-content -->
 
 	<?php
