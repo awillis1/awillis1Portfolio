216300cad61ce1a7bd876c6d5c5f59073d9fe18e
Student <student@CVS-R107D036.nic.bc.ca>
2018-12-07 14:13:30 -0800

    modifies functions.php

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -13,3 +13,18 @@ add_action( 'wp_enqueue_scripts', 'my_google_fonts' );
 function my_google_fonts() {
     wp_enqueue_style( 'my-google-fonts', 'https://fonts.googleapis.com/css?family=Montserrat:200,400,500,600,800', false );
 }
+
+function obscure_login_errors(){
+  return "Staahhhp... I coulda dropped my crossaint.";
+}
+add_filter( 'login_errors', 'obscure_login_errors' );
+
+// custom admin login logo
+function custom_login_logo() {
+	echo '<style type="text/css">
+	h1 a { background-image: url('.get_bloginfo('template_directory').'/images/custom-login-logo.png) !important; }
+	</style>';
+}
+add_action('login_head', 'custom_login_logo');
+
+
