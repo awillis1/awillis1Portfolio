1ba66e0a2d31f860f7da5ce116daa36b92a56bbf
Student <student@CVS-R107D036.nic.bc.ca>
2018-12-14 14:27:05 -0800

    puts posts on portfolio page into grid

--- wp-content/themes/twentyseventeen-child/template-parts/page/content-page.php
+++ wp-content/themes/twentyseventeen-child/template-parts/page/content-page.php
@@ -16,7 +16,7 @@
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php twentyseventeen_edit_link( get_the_ID() ); ?>
-		<?php get_sidebar(); ?>
+		<!-- <?php //get_sidebar(); ?> -->
 
 	</header><!-- .entry-header -->
 	<div class="entry-content">
